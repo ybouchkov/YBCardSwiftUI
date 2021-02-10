c79e4eb579fae60162481b692ba8504af121c4e3
Yani Buchkov <ybuchkov@tfxcorp.com>
2021-02-10 14:12:04 +0200

    First Commit - starter

---
+++ YBCardSwiftUI/YBCardSwiftUI/YBCardSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YBCardSwiftUIApp.swift
+//  YBCardSwiftUI
+//
+//  Created by Yani Buchkov on 10.02.21.
+//
+
+import SwiftUI
+
+@main
+struct YBCardSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
